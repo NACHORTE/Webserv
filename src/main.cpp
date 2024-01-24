4bf8dc753f9e83d9b47df32f4ab44d7cbf73626d
NACHORTE <ignaciortegape@gmail.com>
2024-01-24 10:52:28 +0100

    [main]<2024-01-24-10:52> while faster

--- src/main.cpp
+++ src/main.cpp
@@ -148,22 +148,17 @@ int main(int argc, char **argv)
 						std::cout << "[SERVER] Error fcntl\n";
 						return 1;
 					}
-					/*while (poll(&fds[0],fds_size, 1000) > 0 && (fds[i].revents & POLLIN) && ((n_read = read(fds[i].fd, buff, BUFF_SIZE)) > 0))  //NOTE do while for little buff
+					while (poll(&fds[0],fds_size, 1) > 0 && (fds[i].revents & POLLIN) && ((n_read = read(fds[i].fd, buff, BUFF_SIZE)) > 0))  //NOTE do while for little buff
 					{
 						std::cout << "n_read: " << (int)n_read << "\n";
 						msg += std::string(buff,n_read);
 					}
-					std::cout << "fuera while\n";*/
-					while ((n_read = read(fds[i].fd, buff, BUFF_SIZE)) > 0)
-					{
-						std::cout << "n_read: " << (int)n_read << "\n";
-						msg += std::string(buff,n_read);
-					}
-					/*if (n_read < 0)
+					std::cout << "fuera while\n";
+					if (n_read < 0)
 					{
 						std::cout << "[SERVER] Error reading from socket\n";
 						return 1;
-					}*/
+					}
 					/*size_t msg_size = msg.size();
 					std::cout << RED "buff:"<< "\n";
 					for (size_t i = 0; i < msg_size; i++)
