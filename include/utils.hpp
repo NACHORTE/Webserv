110e0141f8e5ad15b74762b33a17537c57dc47a0
shuoscaryo <shuoscaryo@gmail.com>
2024-01-25 23:42:39 +0100

    [dev]<2024-01-25-23:42> utils: Added Cool description messages to functions, and added removeQuotes function

--- include/utils.hpp
+++ include/utils.hpp
@@ -4,11 +4,13 @@
 
 std::string int_to_string(int n);
 std::string readFile(const std::string& filePath, bool binary = false);
-std::string trim(const std::string& str);
+std::string trim(const std::string& str, const std::string & charset = " \t\n\r\f\v");
 int ok_config(std::string config_file);
 char back(std::string str);
 void pop_back(std::string& str);
 std::string getExtension(const std::string & filename);
-std::string getContentType(const std::string & filename);
+std::string extToMime(const std::string & filename);
+std::string mimeToExt(const std::string & mime);
 bool isBinaryFile(const std::string & filename);
 std::map<std::string,std::string> get_params(const std::string & str, char separator = ';', char eq = '=');
+std::string removeQuotes(const std::string& str);
--- src/utils.cpp
+++ src/utils.cpp
@@ -5,6 +5,43 @@
 #include <vector>
 #include <map>
 
+static std::map<std::string, std::string> _ext_mime_map;
+
+static void initext_mime_map()
+{
+	_ext_mime_map["txt"] = "text/plain";
+	_ext_mime_map["htm"] = "text/html";
+	_ext_mime_map["html"] = "text/html";
+	_ext_mime_map["css"] = "text/css";
+	_ext_mime_map["js"] = "application/javascript";
+	_ext_mime_map["jpg"] = "image/jpeg";
+	_ext_mime_map["jpeg"] = "image/jpeg";
+	_ext_mime_map["png"] = "image/png";
+	_ext_mime_map["gif"] = "image/gif";
+	_ext_mime_map["swf"] = "application/x-shockwave-flash";
+	_ext_mime_map["txt"] = "text/plain";
+	_ext_mime_map["pdf"] = "application/pdf";
+	_ext_mime_map["mp3"] = "audio/mpeg";
+	_ext_mime_map["mp4"] = "video/mp4";
+	_ext_mime_map["avi"] = "video/x-msvideo";
+	_ext_mime_map["mpeg"] = "video/mpeg";
+	_ext_mime_map["ico"] = "image/x-icon";
+	_ext_mime_map["bin"] = "application/octet-stream";
+}
+
+/**
+ * @brief Converts an integer to a string.
+ * 
+ * This function takes an integer as input and returns a string representing that number.
+ * 
+ * @param n Integer to convert to a string.
+ * @return A string representing the integer.
+ * 
+ * @example
+ * int number = 42;
+ * std::string result = int_to_string(number);
+ * // Expected result: "42"
+ */
 std::string int_to_string(int n)
 {
 	std::stringstream ss;
@@ -12,7 +49,23 @@ std::string int_to_string(int n)
 	return ss.str();
 }
 
-std::string readFile(const std::string& filePath, bool binary) {
+/**
+ * @brief Reads the contents of a file and returns them as a string.
+ * 
+ * This function opens a file specified by the given file path and reads its contents into a string.
+ * 
+ * @param filePath Path to the file to be read.
+ * @param binary Indicates whether to open the file in binary mode.
+ * @return A string containing the contents of the file.
+ * 
+ * @throws std::runtime_error If the file cannot be opened.
+ * 
+ * @example
+ * std::string filePath = "example.txt";
+ * std::string fileContent = readFile(filePath, false);
+ */
+std::string readFile(const std::string& filePath, bool binary)
+{
 	// Open the file in binary or text mode
     std::ifstream file(filePath.c_str(), binary ? std::ios::binary : std::ios::in);
 
@@ -23,8 +76,8 @@ std::string readFile(const std::string& filePath, bool binary) {
 	// Read the file into a string
     std::ostringstream htmlContent;
     htmlContent << file.rdbuf();
+
     return htmlContent.str();
-	// The file closes automatically when it goes out of scope
 }
 
 int ok_config(std::string config_file)
@@ -44,25 +97,79 @@ int ok_config(std::string config_file)
 	return 0;
 }
 
-std::string trim(const std::string& str)
+/**
+ * @brief Trims specified characters from the beginning and end of a string.
+ * 
+ * This function removes characters specified in the given charset from the beginning and end of the input string.
+ * If no charset is provided, it trims whitespace characters by default.
+ * 
+ * @param str The input string to be trimmed.
+ * @param charset The characters to be trimmed from the string (default is whitespace).
+ * @return A string with specified characters removed from the beginning and end.
+ * 
+ * @example
+ * std::string input = "   example   ";
+ * std::string trimmed = trim(input);
+ * // Result: "example"
+ */
+std::string trim(const std::string& str, const std::string & charset)
 {
-    size_t first = str.find_first_not_of(" \t\r\n");
+    size_t first = str.find_first_not_of(charset);
     if (std::string::npos == first)
         return str;
-    size_t last = str.find_last_not_of(" \t\r\n");
+    size_t last = str.find_last_not_of(charset);
     return str.substr(first, (last - first + 1));
 }
 
+/**
+ * @brief Returns the last character of a string.
+ * 
+ * This function takes a string as input and returns its last character.
+ * 
+ * @param str The input string.
+ * @return The last character of the string.
+ * 
+ * @example
+ * std::string text = "hello";
+ * char lastChar = back(text);
+ * // Result: 'o'
+ */
 char back(std::string str)
 {
 	return str[str.length() - 1];
 }
 
+/**
+ * @brief Removes the last character from a string.
+ * 
+ * This function modifies the input string by removing its last character.
+ * 
+ * @param str The string to remove the last character from.
+ * 
+ * @example
+ * std::string text = "hello";
+ * pop_back(text);
+ * // Result: text is now "hell"
+ */
 void pop_back(std::string& str)
 {
 	str.erase(str.length() - 1);
 }
 
+/**
+ * @brief Retrieves the file extension from a filename.
+ * 
+ * This function returns the file extension from the given filename. If no extension is found or the filename ends with '.', 
+ * an empty string is returned.
+ * 
+ * @param filename The filename from which to extract the extension.
+ * @return The file extension or an empty string if not found.
+ * 
+ * @example
+ * std::string file = "example.txt";
+ * std::string extension = getExtension(file);
+ * // Result: "txt"
+ */
 std::string getExtension(const std::string & filename)
 {
 	// Get the position of the last '.' in the filename
@@ -76,49 +183,100 @@ std::string getExtension(const std::string & filename)
 	return filename.substr(pos + 1);
 }
 
-std::string getContentType(const std::string & filename) {
-	// List of extension-content_type pairs that only initializes once
-	static std::map<std::string, std::string> content_type;
-	if (content_type.empty())
-	{
-		content_type["txt"] = "text/plain";
-		content_type["htm"] = "text/html";
-		content_type["html"] = "text/html";
-		content_type["css"] = "text/css";
-		content_type["js"] = "application/javascript";
-		content_type["jpg"] = "image/jpeg";
-		content_type["jpeg"] = "image/jpeg";
-		content_type["png"] = "image/png";
-		content_type["gif"] = "image/gif";
-		content_type["swf"] = "application/x-shockwave-flash";
-		content_type["txt"] = "text/plain";
-		content_type["pdf"] = "application/pdf";
-		content_type["mp3"] = "audio/mpeg";
-		content_type["mp4"] = "video/mp4";
-		content_type["avi"] = "video/x-msvideo";
-		content_type["mpeg"] = "video/mpeg";
-		content_type["ico"] = "image/x-icon";
-	}
+/**
+ * @brief Converts a file extension to a corresponding MIME type.
+ * 
+ * This function takes a file extension as input and returns the corresponding MIME type.
+ * If there is no extension or its not recognized, it defaults to "application/octet-stream".
+ * 
+ * @param filename The filename with the extension to convert.
+ * @return The MIME type associated with the file extension.
+ * 
+ * @example
+ * std::string file = "document.pdf";
+ * std::string mimeType = extToMime(file);
+ * // Result: "application/pdf"
+ */
+std::string extToMime(const std::string & filename)
+{
+	// Initialize the map if it's empty
+	if (_ext_mime_map.empty())
+		initext_mime_map();
 
 	// Get the extension of the file
 	std::string extension = getExtension(filename);
 
 	// If the extension is not in the map, return "application/octet-stream"
-	if (extension == "" || content_type.count(extension) == 0)
+	if (extension == "" || _ext_mime_map.count(extension) == 0)
 		return "application/octet-stream";
 	
 	// Return the content type
-	return content_type[extension];
+	return _ext_mime_map[extension];
 }
 
+/**
+ * @brief Converts a MIME type to the corresponding file extension.
+ * 
+ * This function takes a MIME type as input and returns the corresponding file extension.
+ * If the MIME type is not recognized, it returns an empty string.
+ * 
+ * @param mime The MIME type to convert.
+ * @return The file extension associated with the MIME type or an empty string if not found.
+ * 
+ * @example
+ * std::string mimeType = "application/pdf";
+ * std::string fileExtension = mimeToExt(mimeType);
+ * // Result: "pdf"
+ */
+std::string mimeToExt(const std::string & mime)
+{
+	// Initialize the map if it's empty
+	if (_ext_mime_map.empty())
+		initext_mime_map();
+
+	// Search the map for the mime type
+	std::map<std::string,std::string>::iterator it;
+	for (it = _ext_mime_map.begin(); it != _ext_mime_map.end(); ++it)
+		if (it->second == mime)
+			return it->first;
+
+	// If the mime type is not in the map, return an empty string
+	return "";
+}
+
+/**
+ * @brief Checks if a file is binary based on its extension.
+ * 
+ * This function determines if a file is binary by examining the MIME associated with its extension.
+ * If the MIME type starts with "text/", it is considered a non-binary file.
+ * 
+ * @param filename The name of the file to check.
+ * @return True if the file is binary, false if it is a text file.
+ * 
+ * @example
+ * std::string file = "image.png";
+ * bool isBinary = isBinaryFile(file);
+ * // Result: true
+ */
 bool isBinaryFile(const std::string & filename)
 {
-	std::string contentType = getContentType(filename);
+	std::string contentType = extToMime(filename);
 	return (contentType.substr(0,5) != "text/");
 }
 
-// Create a map with the pairs key-value in the string, each param split by the separator and each key-value split by the eq
-// Example: "key1=value1;key2=value2" -> {"key1": "value1", "key2": "value2"}
+/**
+ * @brief Parses a string into a map of key-value pairs.
+ * 
+ * Each parameter is split by the specified separator, and each key-value pair is split by the equal sign.
+ * 
+ * @param inputString The input string to be parsed.
+ * @param separator The separator used to split parameters.
+ * @param eq The character used to split key-value pairs.
+ * @return A map containing key-value pairs parsed from the input string.
+ * 
+ * @example
+ * "key1=value1;key2=value2" -> {"key1": "value1", "key2": "value2"}
+ */
 std::map<std::string,std::string> get_params(const std::string & str, char separator, char eq)
 {
 	std::map<std::string,std::string> ret;
@@ -150,4 +308,24 @@ std::map<std::string,std::string> get_params(const std::string & str, char separ
 	} while (end != std::string::npos && end < str.length());
 
 	return ret;
+}
+
+/**
+ * @brief Removes surrounding quotes from a string.
+ * 
+ * This function checks if a string is enclosed in double quotes and removes them if present.
+ * 
+ * @param str The input string.
+ * @return The string with surrounding quotes removed, or the original string if no quotes are found.
+ * 
+ * @example
+ * std::string quotedStr = "\"example\"";
+ * std::string unquotedStr = removeQuotes(quotedStr);
+ * // Result: "example"
+ */
+std::string removeQuotes(const std::string& str)
+{
+	if (str.length() > 1 && str[0] == '"' && str[str.length() - 1] == '"')
+		return str.substr(1, str.length() - 2);
+	return str;
 }
