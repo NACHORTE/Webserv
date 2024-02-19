4ffc09fe548a090e271e29854d9934c19f446cb9
shuoscaryo <shuoscaryo@gmail.com>
2024-02-19 23:24:17 +0100

    [dev]<2024-02-19-23:24> Server class: Structured the header of the class

--- src/class/Server.hpp
+++ src/class/Server.hpp
@@ -16,42 +16,49 @@ class Server
 
 	// Setters and getters
 		void setPort(int port);
-		void setHost(const std::string & host);
-		void setRoot(const std::string & root);
-		void setErrorPage(const std::string & errorPage);
+		void setClientMaxBodySize(int clientMaxBodySize);
 		void setIndex(const std::string & index);
-		void setServerName(const std::string & serverName);
-		void setMaxBody(int maxBody);
-		void addLocation(const t_location & location);
+		void setRoot(const std::string & root);
 
-		int getPort(void) const;
-		const std::string& getServerName(void) const;
-		const std::string& getHost(void) const;
-		const std::string& getRoot(void) const;
-		const std::string& getErrorPage(void) const;
-		const std::string& getIndex(void) const;
-		std::vector<t_location> getLocations(void) const;
-		int getMaxBody(void) const;
+		void addServerName(const std::string & serverName);
+		void addLocation(const t_location & location);
+		void addErrorPage(const std::string & errorPage);
+
+		const int & getPort() const;
+		const int & getClientMaxBodySize() const;
+		const std::string & getIndex() const;
+		const std::string & getRoot() const;
+		const std::vector<std::string> & getServerNames() const;
+		const std::vector<t_location> & getLocations() const;
+		const std::vector<std::string> & getErrorPages() const;
 
 	// Member functions
 
 	// Operator overloads
 		Server & operator=(const Server & rhs);
+
 	protected:
 	private:
-		std::map<std::string, HttpResponse (*)(const HttpRequest &, const Locations &)> _allowed_methods;
-		Locations _allowed_paths;
+	// Member attributes
+		// Port number that the server will listen to
+		int _port;
+		// Maximum body size that the server will accept
+    	int _clientMaxBodySize;
+		// Default file to serve when the request is for a directory
+    	std::string _index;
+		// Root directory for the server
+    	std::string _root;
+		// List of server names that the server will respond to (host header in the request must match one of these names)
+    	std::vector<std::string> _serverNames;
+		// List of locations for the server
+		std::vector<t_location> _locations;
+		// List of error pages for the server
 		std::vector<std::string> _errorPages;
-		int port;
-    	std::string sv_name;
-    	std::string host;
-    	std::string root;
-    	std::string error_page;
-    	std::string index;
-		std::vector<t_location> locations;
-    	int max_body;
-		
-	friend std::ostream &operator<<(std::ostream &os, const Server &obj);
+
+	// Private member functions
+
+	// Friends <3
+		friend std::ostream &operator<<(std::ostream &os, const Server &obj);
 };
 
 std::ostream &operator<<(std::ostream &os, const Server &obj);
