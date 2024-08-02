8fec6ff7d24efa5fcea937d688722bb63c5d542b
shuoscaryo <shuoscaryo@gmail.com>
2024-08-02 18:10:07 +0200

    [dev]<2024-08-02-18:10> This should be rock solid now

--- src/class/HttpRequest.cpp
+++ src/class/HttpRequest.cpp
@@ -3,6 +3,11 @@
 #include "utils.hpp"
 #include "colors.h"
 
+/**
+ * @brief Default Constructor for the HttpRequest class.
+ * 
+ * Initializes the request as not ready, the header as not ready, and no error.
+ */
 HttpRequest::HttpRequest()
 {
 	_requestReady = false;
@@ -10,43 +15,146 @@ HttpRequest::HttpRequest()
 	_error = false;
 }
 
+/**
+ * @brief Constructor for the HttpRequest class.
+ * 
+ * Initializes the request with the input string. If the string contains a
+ * complete request, the request is set as ready. Otherwise it gets added to
+ * the buffer, waiting for more data.
+ * 
+ * If any error occurs, the error flag is set.
+ * 
+ * @param msg The string containing the http request or part of it.
+ */
 HttpRequest::HttpRequest(const std::string& msg)
 {
 	_requestReady = false;
 	_headerReady = false;
 	_error = false;
-	parse(msg);
+	addData(msg);
 }
 
+/**
+ * @brief Copy Constructor for the HttpRequest class.
+ * 
+ * Initializes the request as a copy of another request.
+ * 
+ * @param other The HttpRequest to copy.
+ */
 HttpRequest::HttpRequest(const HttpRequest& other)
 {
 	*this = other;
 }
 
+/**
+ * @brief Destructor for the HttpRequest class.
+ * 
+ * Does nothing.
+ */
 HttpRequest::~HttpRequest()
 {}
 
+/**
+ * @brief Set the method of the request.
+ * 
+ * @param method The method to set.
+ */
 void HttpRequest::setMethod(const std::string& method)
 {
 	this->_method = method;
 }
 
+/**
+ * @brief Get the method of the request.
+ * 
+ * @return The method of the request.
+ */
+const std::string & HttpRequest::getMethod() const
+{
+	return _method;
+}
+
+/**
+ * @brief Set the path of the request.
+ * 
+ * If there is a query string, it is separated from the path and stored in
+ * the _queryString attribute (use getQueryString() to get it).
+ * 
+ * The path and query string are decoded from URL formatting and the path is
+ * cleaned from extra slashes or dots.
+ * 
+ * @param path The path to set.
+ */
 void HttpRequest::setPath(const std::string& path)
 {
-	this->_path = path;
+	size_t pos = path.find("?");
+	if (pos != std::string::npos)
+	{
+		_queryString = path.substr(pos + 1);
+		_path = path.substr(0, pos);
+	}
+	// Decode path and query string
+	_path = decodeURL(_path);
+	_queryString = decodeURL(_queryString);
+	// Clean path
+	_path = cleanPath(_path);
+
 }
 
-void HttpRequest::setVersion(const std::string& version)
+/**
+ * @brief Get the path of the request.
+ * 
+ * If the path had a query string, it is not included in the path.
+ * 
+ * The returned path is decoded from URL formatting and cleaned from extra
+ * slashes or dots.
+ * 
+ * @return The path of the request.
+ */
+const std::string & HttpRequest::getPath() const
 {
-	this->_version = version;
+	return _path;
 }
 
+/**
+ * @brief Get the query string of the request.
+ * 
+ * The query string is decoded from URL formatting.
+ * 
+ * Does not include the ? character.
+ * 
+ * @return The query string of the request.
+ */
+const std::string & HttpRequest::getQueryString() const
+{
+	return _queryString;
+}
+
+/**
+ * @brief Set a header in the request.
+ * 
+ * Multiple headers with the same key are allowed.
+ * 
+ * If a header with the same key already exists, it is not replaced.
+ * 
+ * @param key The key of the header.
+ * @param value The value of the header.
+ * 
+ * @example setHeader("Content-Type", "text/html");
+ */
 void HttpRequest::setHeader(const std::string& key, const std::string& value)
 {
 	// Multiple headers with the same key are allowed
 	_headers.push_back(std::make_pair(key, value));
 }
 
+/**
+ * @brief Unset a header name from the request.
+ * 
+ * Removes all headers with the given key.
+ * 
+ * @param key The key of the header to remove.
+ */
 void HttpRequest::unsetHeader(const std::string& key)
 {
 	std::vector<std::pair<std::string, std::string> >::iterator it;
@@ -59,27 +167,122 @@ void HttpRequest::unsetHeader(const std::string& key)
 	}
 }
 
-void HttpRequest::setBody(const std::string& body)
+/**
+ * @brief Get all headers with a given key.
+ * 
+ * @param key The key of the headers to get.
+ * 
+ * @return A vector with the contents of every header with the key.
+ */
+std::vector<std::string> HttpRequest::getHeader(const std::string& key) const
 {
-	this->_body = body;
+	std::vector<std::string> output;
+
+	size_t header_size = _headers.size();
+	for (size_t i = 0; i < header_size; i++)
+		if (_headers[i].first == key)
+			output.push_back(_headers[i].second);
+	return output;
 }
 
-long long int getChunk(std::string & buf, std::string & body)
+/**
+ * @brief Get all headers of the request.
+ * 
+ * Maybe used to iterate over all headers from outside the class.
+ * 
+ * @return A reference to the headers of the request.
+ */
+const std::vector<std::pair<std::string, std::string> > & HttpRequest::getHeaders() const
+{
+	return _headers;
+}
+
+/**
+ * @brief Set the body of the request.
+ * 
+ * The body is set as is, no decoding or cleaning is done.
+ * 
+ * The headers Content-Length and Transfer-Encoding are updated accordingly.
+ * 
+ * @param body The body to set.
+ * @param chunked (default = false) If true, the body is sent as chunked.
+ */
+void HttpRequest::setBody(const std::string& body, bool chunked)
+{
+	this->_body = body;
+	unsetHeader("Transfer-Encoding");
+	unsetHeader("Content-Length");
+	if (chunked == false)
+		setHeader("Content-Length", intToString(body.size()));
+	else
+		setHeader("Transfer-Encoding", "chunked");
+}
+
+/**
+ * @brief Set the request as ready or not.
+ * 
+ * Used when manually setting the request. If the request is set as ready,
+ * it is considered complete and can be sent.
+ * 
+ * Don't use this function unless you know what you are doing!
+ * @param ready The value to set.
+ */
+void HttpRequest::requestReady(bool ready)
+{
+	_requestReady = ready;
+}
+
+/**
+ * @brief Check if the request is ready.
+ * 
+ * @return True if the request is ready, false otherwise.
+ */
+bool HttpRequest::requestReady() const
+{
+	return _requestReady;
+}
+
+/**
+ * @brief Get the body of the request.
+ * 
+ * @return The body of the request.
+ */
+const std::string & HttpRequest::getBody() const
+{
+	return _body;
+}
+
+/**
+ * @brief Extract a chunk from the buffer and add it to the body.
+ * 
+ * If the buffer is not long enough to read a chunk, return 0.
+ * 
+ * If there is any error reading the chunk, return -1.
+ * 
+ * If the chunk is read successfully, return the size of the chunk read.
+ * 
+ *
+ * @param buf The buffer to read from
+ * @param body The body to add the chunk to
+ * @return The size of the chunk read
+ */
+static long long int getChunk(std::string & buf, std::string & body)
 {
 	//get the length of the chunk
 	size_t chunkSize;
 	size_t pos = buf.find("\r\n");
+	// if the buff is not long enough return 0, else if it is more than 8HEX + \r return -1
 	if (pos == std::string::npos)
-		return -1;
+		return (buf.size() >= 10 ? -1 : 0);
 	std::istringstream iss(buf.substr(0, pos));
 	iss >> std::hex >> chunkSize;
 	if (iss.fail() or not iss.eof())
 		return -1;
 
-	// if the buffer is not long enough, return error
+	// if the buffer is not long enough, skip
 	// hex size + \r\n + chunkSize + \r\n
-	if (pos + chunkSize + 4 > buf.size())
-		return -1;
+	if (buf.size() < pos + chunkSize + 4)
+		return 0;
 	
 	// add the chunk to the body and remove it from the buffer
 	body += buf.substr(pos + 2, chunkSize);
@@ -89,6 +292,26 @@ long long int getChunk(std::string & buf, std::string & body)
 	return pos + chunkSize + 4;
 }
 
+/**
+ * @brief Add a piece of data to the request.
+ * 
+ * Main function used to parse the request. If the data is long enough to
+ * contain a complete request, only one call is needed. Otherwise, multiple
+ * calls are needed.
+ * 
+ * Can handle chunked encoding.
+ * 
+ * Keeps parsing data from the input until the request is ready.
+ * 
+ * Returns the number of bytes read from the input each time it is called.
+ * 
+ * If the request is ready, it returns 0 because it doesn't read anything.
+ * 
+ * If an error occurs, it returns -1.
+ * 
+ * @param data The data to add to the request.
+ * @return The number of bytes read.
+ */
 long long int HttpRequest::addData(const std::string & data)
 {
 	if (_requestReady)
@@ -105,7 +328,7 @@ long long int HttpRequest::addData(const std::string & data)
 			return data.size();
 		// If the end of the header is found, parse it
 		if (parseHeader(_buffer.substr(0, pos + 4)) == 1)
-			return (_error=true, -1);
+			return (this->clear(), _error=true, -1);
 		_buffer = _buffer.substr(pos + 4);
 		_headerReady = true;
 	}
@@ -125,11 +348,11 @@ long long int HttpRequest::addData(const std::string & data)
 				std::istringstream iss(contentLengthHeader[0]);
 				iss >> contentLength;
 				if (iss.fail() or not iss.eof())
-					return (_error=true, -1);
+					return (this->clear(), _error=true, -1);
 			}
 			// If no content length is found, or some weird error occurs, return
 			if (contentLength < _body.size())
-				return (_error=true, -1);
+				return (this->clear(), _error=true, -1);
 			// Add the data to the body
 			size_t len = (contentLength - _body.size() > _buffer.size()) ?
 				_buffer.size() : contentLength - _body.size();
@@ -143,153 +366,94 @@ long long int HttpRequest::addData(const std::string & data)
 		//is chunked
 		else
 		{
- 			try
+			long long int chunkSize;
+			do
 			{
-				long long int chunkSize;
-				do
+				chunkSize=getChunk(_buffer, _body);
+				// 0\r\n\r\n
+				if (chunkSize == 5)
 				{
-					chunkSize=getChunk(_buffer, _body);
-					// 0\r\n\r\n
-					if (chunkSize == 5)
-					{
-						_requestReady = true;
-						unsetHeader("Transfer-Encoding");
-						setHeader("Content-Length", intToString(_body.size()));
-					}
-				} while (chunkSize > 5);
-			}
-			catch(const std::exception& e)
-			{
-				return (_error=true, -1);
-			}
+					_requestReady = true;
+					unsetHeader("Transfer-Encoding");
+					unsetHeader("Content-Length");
+					setHeader("Content-Length", intToString(_body.size()));
+				}
+				else if (chunkSize == -1)
+					return (this->clear(), _error=true, -1);
+			} while (chunkSize > 5);
 		}
 	}
 
 	return data.size() - _buffer.size(); 
 }
 
-const std::string & HttpRequest::getMethod() const
-{
-	return _method;
-}
-
-const std::string & HttpRequest::getPath() const
-{
-	return _path;
-}
-
-const std::string & HttpRequest::getVersion() const
-{
-	return _version;
-}
-
-std::vector<std::string> HttpRequest::getHeader(const std::string& key) const
-{
-	std::vector<std::string> output;
-
-	size_t header_size = _headers.size();
-	for (size_t i = 0; i < header_size; i++)
-		if (_headers[i].first == key)
-			output.push_back(_headers[i].second);
-	return output;
-}
-
-std::vector<std::pair<std::string, std::string> > HttpRequest::getHeaders() const
-{
-	return _headers;
-}
-
-const std::string & HttpRequest::getBody() const
-{
-	return _body;
-}
-
-void HttpRequest::parse(const std::string& msg)
-{
-	// Clear current request
-	this->clear();
-
-	// Cast string to stream
-	std::istringstream iss(msg);
-
-	// Get first line
-	std::string line;
-	std::getline(iss, line);
-	std::istringstream iss_line(trim(line));
-	iss_line >> _method >> _path >> _version;
-	// Check if line is valid
-	if (iss_line.fail() || !iss_line.eof())
-		return (_error = true, this->clear());
-
-	// Get headers
-	while (std::getline(iss, line) && line != "\r" && line != "")
-	{
-		// Check if line is vali
-		size_t index = line.find(':');
-		if (index == std::string::npos)
-			return (_error = true, this->clear());
-		// Parse line
-		std::string key = trim(line.substr(0, index));
-		std::string value = trim(line.substr(index + 1));
-		_headers.push_back(std::make_pair(key, value));
-	}
-
-	// Get body
-	_body = iss.str().substr(iss.tellg());
-
-	_requestReady = true;
-}
-
+/**
+ * @brief Convert the request to a string.
+ * 
+ * Used to create a string from the request to send it.
+ * If the request is not ready, an empty string is returned.
+ * 
+ * @return The request as a string.
+ */
 std::string HttpRequest::to_string() const
 {
 	if (!_requestReady)
 		return "";
-	std::string ret = _method + " " + _path + " " + _version + "\r\n";
-	size_t header_size = _headers.size();
-	for (size_t i = 0; i < header_size; i++)
+
+	std::string ret;
+	if (_queryString.empty())
+		ret = _method + " " + _path + "HTTP/1.1\r\n";
+	else
+		ret = _method + " " + _path + "?" + _queryString + "HTTP/1.1\r\n";
+
+	for (size_t i = 0; i < _headers.size(); ++i)
 		ret += _headers[i].first + ": " + _headers[i].second + "\r\n";
 	ret += "\r\n" + _body;
+
 	return ret;
 }
 
+/**
+ * @brief Clear all fields of the request.
+ * 
+ * Used to reset the request to its initial state.
+ */
 void HttpRequest::clear()
 {
 	_method.clear();
 	_path.clear();
-	_version.clear();
+	_queryString.clear();
 	_headers.clear();
 	_body.clear();
 	_requestReady = false;
 	_headerReady = false;
 	_error = false;
+	_buffer.clear();
 }
 
-bool HttpRequest::empty() const
-{
-	return (_method.empty()
-		&& _path.empty()
-		&& _version.empty()
-		&& _headers.empty()
-		&& _body.empty());
-}
-
-bool HttpRequest::requestReady() const
-{
-	return _requestReady;
-}
-
+/**
+ * @brief Check if the request is in an error state.
+ * 
+ * @return True if the request is in an error state, false otherwise.
+ */
 bool HttpRequest::error() const
 {
 	return _error;
 }
 
+/**
+ * @brief Copy the request.
+ * 
+ * @param rhs The request to copy.
+ * @return A reference to the copied request.
+ */
 HttpRequest & HttpRequest::operator=(HttpRequest const & rhs)
 {
 	if (this != &rhs)
 	{
 		this->_method = rhs._method;
 		this->_path = rhs._path;
-		this->_version = rhs._version;
+		this->_queryString = rhs._queryString;
 		this->_headers = rhs._headers;
 		this->_body = rhs._body;
 		this->_requestReady = rhs._requestReady;
@@ -300,16 +464,15 @@ HttpRequest & HttpRequest::operator=(HttpRequest const & rhs)
 	return *this;
 }
 
-std::string HttpRequest::operator()() const
-{
-	return to_string();
-}
-
-std::vector<std::string> HttpRequest::operator[](const std::string& key) const
-{
-	return getHeader(key);
-}
-
+/**
+ * @brief Parse the header of the request.
+ * 
+ * Auxiliary function used to parse the header of the request.
+ * Used only by addData.
+ * 
+ * @param header The header to parse.
+ * @return 0 if the header is parsed successfully, 1 otherwise.
+ */
 int HttpRequest::parseHeader(const std::string& header)
 {
 	_headers.clear();
@@ -317,32 +480,25 @@ int HttpRequest::parseHeader(const std::string& header)
 	std::istringstream iss(header);
 
 	// Get first line
+	std::string version;
 	std::string line;
 	std::getline(iss, line);
-	std::istringstream iss_line(trim(line));
-	iss_line >> _method >> _path >> _version;
+	std::istringstream iss_line(line); // NOTE trim removed, check if still works
+	iss_line >> _method >> _path >> version;
 	// Check if line is valid
 	if (iss_line.fail() || !iss_line.eof())
-		return (_method.clear(), _path.clear(), _version.clear(), _headers.clear(), 1);
-	// Decode path and query string if needed
-	_path = decodeURL(_path);
-	// Split path and query string
-	size_t pos = _path.find("?");
-	if (pos != std::string::npos)
-	{
-		_queryString = _path.substr(pos + 1);
-		_path = _path.substr(0, pos);
-	}
-	// Clean path
-	_path = cleanPath(_path);
+		return 1;
+	// Use setPath to parse the path and query string
+	setPath(_path);
+
 
 	// Get headers
 	while (std::getline(iss, line) && line != "\r" && line != "")
 	{
-		// Check if line is vali
+		// Check if line is valid
 		size_t index = line.find(':');
 		if (index == std::string::npos)
-			return (_method.clear(), _path.clear(), _version.clear(), _headers.clear(), 1);
+			return 1;
 		// Parse line
 		std::string key = trim(line.substr(0, index));
 		std::string value = trim(line.substr(index + 1));
@@ -352,6 +508,18 @@ int HttpRequest::parseHeader(const std::string& header)
 	return 0;
 }
 
+/**
+ * @brief Class printf!
+ * 
+ * Used to print the request to the output stream.
+ * 
+ * If the request is ready, it prints the request as a string.
+ * Otherwise, it prints the attributes of the request.
+ * 
+ * @param o The output stream to print to.
+ * @param rhs The request to print.
+ * @return The output stream.
+ */
 std::ostream & operator<<(std::ostream & o, HttpRequest const & rhs)
 {
 	if (rhs._requestReady)
@@ -361,13 +529,12 @@ std::ostream & operator<<(std::ostream & o, HttpRequest const & rhs)
 	}
 	o << CYAN << "_method: " << RESET << rhs._method << std::endl;
 	o << CYAN << "_path: " << RESET << rhs._path << std::endl;
-	o << CYAN << "_version: " << RESET << rhs._version << std::endl;
+	o << CYAN << "_queryString: " << RESET << rhs._queryString << std::endl;
 	o << CYAN << "_headers: " << RESET << rhs._headers.size() << std::endl;
 	for (size_t i = 0; i < rhs._headers.size(); i++)
-		o << rhs._headers[i].first << ": " << rhs._headers[i].second << std::endl;
+		o << "\t" << rhs._headers[i].first << ": " << rhs._headers[i].second << std::endl;
 	o << CYAN << "_body: " << RESET << rhs._body << std::endl;
 	o << CYAN << "_buffer: " << RESET << rhs._buffer << std::endl;
-
 	o << CYAN << "_requestReady: " << RESET << rhs._requestReady << std::endl;
 	o << CYAN << "_headerReady: " << RESET << rhs._headerReady << std::endl;
 	o << CYAN << "_error: " << RESET << rhs._error << std::endl;
--- src/class/HttpRequest.hpp
+++ src/class/HttpRequest.hpp
@@ -16,45 +16,41 @@ class HttpRequest {
 	public:
 		// constructors, destructor, copy constructor
 
-		HttpRequest();	// default constructor sets all fields to empty
-		HttpRequest(const std::string& msg); // parse a string into an HttpRequest
-		HttpRequest(HttpRequest const & src); // copy constructor
-		~HttpRequest(); // destructor does nothing
+		HttpRequest();
+		HttpRequest(const std::string& msg);
+		HttpRequest(HttpRequest const & src);
+		~HttpRequest();
 
 		// getters, setters
 
 		void setMethod(const std::string& method);
+		const std::string & getMethod() const;
+
 		void setPath(const std::string& path);
-		void setVersion(const std::string& version);
+		const std::string & getPath() const;
+		
+		const std::string & getQueryString() const;
+
 		void setHeader(const std::string& key, const std::string& value);
 		void unsetHeader(const std::string& key);
-		void setBody(const std::string& body);
-
-		// Add data to the request buffer and return the number of bytes added
-		long long int addData(const std::string & data);
-
-		const std::string & getMethod() const;
-		const std::string & getPath() const;
-		const std::string & getQueryString() const;
-		const std::string & getVersion() const;
 		std::vector<std::string> getHeader(const std::string& key) const; // returns a vector of all headers with the given key
-		std::vector<std::pair<std::string, std::string> > getHeaders() const;
+		const std::vector<std::pair<std::string, std::string> > & getHeaders() const;
+
+		void setBody(const std::string& body, bool chunked = false);
 		const std::string & getBody() const;
 
+		void requestReady(bool ready);
+		bool requestReady() const;
 		// member functions
 
-		void parse(const std::string& msg); // parse a string into an HttpRequest
+		long long int addData(const std::string & data);
 		std::string to_string() const; // convert an HttpRequest to a string
 		void clear(); // clear all fields
-		bool empty() const; // check if all fields are empty
-		bool requestReady() const; // check if the request is complete
 		bool error() const; // check if the request is in an error state
 
 		// operator overloads
 
 		HttpRequest & operator=(HttpRequest const & rhs);
-		std::string operator()() const; // equivalent to to_string()
-		std::vector<std::string> operator[](const std::string& key) const; // equivalent to getHeader(key)
 
 	protected:
 	private:
@@ -63,7 +59,6 @@ class HttpRequest {
 		std::string _method;	// GET, POST, PUT, DELETE, HEAD, CONNECT, OPTIONS, TRACE, PATCH
 		std::string _path;	// /path/to/file
 		std::string _queryString;	// key=value&key=value...
-		std::string _version;	// HTTP/1.1
 		std::vector<std::pair<std::string, std::string> > _headers;	// key-value pairs
 		std::string _body;	// body of the request
 	
@@ -71,7 +66,7 @@ class HttpRequest {
 		bool _headerReady;
 		bool _error;
 
-		std::string _buffer;
+		std::string _buffer; // Used while adding data to the request
 
 	// Private member functions
 
@@ -81,5 +76,4 @@ class HttpRequest {
 		friend std::ostream & operator<<(std::ostream & o, HttpRequest const & rhs);
 };
 
-// prints the request using to_string and trimming the body if it is too long
 std::ostream & operator<<(std::ostream & o, HttpRequest const & rhs); 
